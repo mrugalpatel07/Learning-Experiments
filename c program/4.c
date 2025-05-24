2e41dfd1d94a28a5a2e1f0c12be29fccbc5bc3cc
mrugalpatel07 <mrugal2722@gmail.com>
2025-05-24 14:31:52 +0530

    Add files via upload

---
+++ c program/4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("\n enter the number= ");
+	scanf("%d",&num);
+	
+	 if(num>0)
+	 {
+	
+	 printf("\n%d positive",num);
+}
+	 else
+	 {
+	 
+	 printf("\n%d negative",num);
+}
+	return 0;
+}
